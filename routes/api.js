dd571cf39aa9880bc5c9959857cce461503c1019
ra8200 <ra8200@hotmail.com>
2021-07-15 16:47:54 -0400

    add code to api.js file

--- routes/api.js
+++ routes/api.js
@@ -8,4 +8,32 @@ appRouter.get("/api/notes",function(req,res){
     console.log("Get",db)
     res.json(db)
 })
+appRouter.post("/api/notes",function(req,res){
+    const recentNote = {
+        id: Math.floor(Math.random()*100),
+        title: req.body.title,
+        test:req.body.text
+    }
 
+    db.push(recentNote)
+    fs.writeFileSync("./db/db.json",JSON.stringify(db),function(err){
+        if (err) throw err;
+    })
+    console.log("POST",db)
+     res.json(db)
+})
+appRouter.delete("/api/notes/:id",function(req,res){
+    var updatedNotes = []
+    for(let i = 0; i < db.length; i++) {
+        if(db[i].id != req.params.id){
+            updatedNotes.push(db[i])
+        }
+    }
+    db = updatedNotes
+    fs.writeFileSync("./db/db.json",JSON.stringify(db),function(err){
+        if (err) throw err;
+    })
+    console.log("POST",db)
+     res.json(db)
+})
+module.exports = appRouter
